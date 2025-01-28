ab96779eb57b1c05b979f50512698bd6b57e6090
Jun Soo Kim <jskim7018@gmail.com>
2025-01-28 20:27:30 +0900

    20250128 - Leetcode Study 14

---
+++ algorithm_study/20250128/LC_217.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        st = set()
+
+        for num in nums:
+            if num in st:
+                return True
+            st.add(num)
+
+        return False
