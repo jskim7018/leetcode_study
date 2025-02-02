7b67e7c95c60b38b8b504684b01b707093ac7911
Jun Soo Kim <jskim7018@gmail.com>
2025-02-02 21:55:42 +0900

    20250202 - Leetcode Study 3-2

--- algorithm_study/20250204/sql/LC_2205.sql
+++ algorithm_study/20250204/sql/LC_2205.sql
@@ -1,9 +1,11 @@
-CREATE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT) RETURNS INT
+CREATE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT) 
+RETURNS INT
 BEGIN
   RETURN (
-      SELECT IFNULL(COUNT(user_id),0) FROM Purchases
-      WHERE time_stamp >= startDate AND time_stamp <= endDate
-      GROUP BY user_id
-      HAVING SUM(amount) >= minAmount
+        SELECT COUNT(DISTINCT user_id) 
+        FROM Purchases
+        WHERE time_stamp >= startDate
+        AND time_stamp <= endDate
+        AND amount >= minAmount
   );
-END
+END;
