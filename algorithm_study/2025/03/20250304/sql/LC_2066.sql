22b578f6a945640c52325a6ac8bc61494e2bc967
Jun Soo Kim <jskim7018@gmail.com>
2025-02-07 23:32:03 +0900

    20250207 - Leetcode Study 6

---
+++ algorithm_study/2025/03/20250304/sql/LC_2066.sql
@@ -0,0 +1,13 @@
+SELECT 
+    account_id, 
+    day, 
+    SUM(
+        IF(type = 'Deposit', amount, -amount)
+    ) OVER (
+        PARTITION BY account_id 
+        ORDER BY day
+    ) AS balance
+FROM Transactions
+ORDER BY 
+    account_id ASC, 
+    day ASC;
