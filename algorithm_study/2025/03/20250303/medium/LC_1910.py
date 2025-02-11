ca1420d2f26738b6bcbaa086f6953ed9bfc03be0
Jun Soo Kim <jskim7018@gmail.com>
2025-02-11 15:05:33 +0900

    20250211 - Leetcode Study 2

--- algorithm_study/2025/03/20250303/medium/LC_1910.py
+++ algorithm_study/2025/03/20250303/medium/LC_1910.py
@@ -1,13 +1,14 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
         stck = list()
+        part = list(part)
         n = len(s)
         m = len(part)
 
         for i in range(n):
             stck.append(s[i])
 
-            if len(stck) >= m and ''.join(stck[len(stck)-m:]) == part:
+            if len(stck) >= m and stck[len(stck)-m:] == part:
                 for j in range(m):
                     stck.pop()
 
