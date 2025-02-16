061cb964f8445245f5cefd89d6f7c639174011bd
Jun Soo Kim <jskim7018@gmail.com>
2025-02-16 14:29:37 +0900

    20250215 - Leetcode Study 2

---
+++ algorithm_study/2025/03/20250313/medium/LC_1718.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class Solution:
+    def constructDistancedSequence(self, n: int) -> List[int]:
+
+
+        used = set()
+        ans = []
+        def construct(idx, seq) -> bool:
+            nonlocal ans
+            if idx >= len(seq):
+                if -1 not in seq:
+                    ans = list(seq)
+                    return True
+                else:
+                    return False
+            if seq[idx] != -1:
+                isFound = construct(idx+1, seq)
+                if isFound:
+                    return isFound
+            else:
+                for i in range(n, 0, -1):
+                    if i not in used:
+                        if (i != 1 and idx+i >= len(seq)) or (i != 1 and seq[idx+i] != -1):
+                            continue
+                        used.add(i)
+                        seq[idx] = i
+                        if i != 1:
+                            seq[idx+i] = i
+                        isFound = construct(idx+1, seq)
+                        used.remove(i)
+                        seq[idx] = -1
+                        if i != 1:
+                            seq[idx+i] = -1
+                        if isFound:
+                            return isFound
+            return False
+
+        seq = [-1] * (n * 2 - 1)
+        construct(0, seq)
+        return ans
