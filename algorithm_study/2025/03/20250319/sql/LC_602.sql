f2966b1fff7647d3324b2845a224cf80aa22fa77
Jun Soo Kim <jskim7018@gmail.com>
2025-02-25 19:39:04 +0900

    20250225 - Leetcode Study 4

---
+++ algorithm_study/2025/03/20250319/sql/LC_602.sql
@@ -0,0 +1,15 @@
+SELECT id, SUM(num) AS num  
+FROM (  
+    SELECT requester_id AS id, COUNT(*) AS num  
+    FROM RequestAccepted  
+    GROUP BY requester_id  
+
+    UNION ALL  
+
+    SELECT accepter_id AS id, COUNT(*) AS num  
+    FROM RequestAccepted  
+    GROUP BY accepter_id  
+) AS IdCount  
+GROUP BY id  
+ORDER BY num DESC  
+LIMIT 1;
