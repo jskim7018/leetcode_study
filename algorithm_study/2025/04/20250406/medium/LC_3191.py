433e736f83abc01d292e545e32073faea6c2b6ce
Jun Soo Kim <jskim7018@gmail.com>
2025-03-19 19:17:23 +0900

    20250319 - Leetcode Study 1

---
+++ algorithm_study/2025/04/20250406/medium/LC_3191.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        ans = 0
+        for i in range(n-2):
+            if nums[i] == 0:
+                nums[i] = nums[i] != 1
+                nums[i+1] = nums[i+1] != 1
+                nums[i+2] = nums[i+2] != 1
+
+                ans += 1
+
+        if nums[n-1] == 0 or nums[n-2] == 0:
+            return -1
+        else:
+            return ans
