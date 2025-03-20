c24cdf5efe3133b644bf76868193dbda465c5ce8
Jun Soo Kim <jskim7018@gmail.com>
2025-03-20 16:39:21 +0900

    20250320 - Leetcode Study 1-2

--- algorithm_study/2025/03/20250320/hard/LC_3108.py
+++ algorithm_study/2025/03/20250320/hard/LC_3108.py
@@ -6,43 +6,38 @@ class Solution:
     def minimumCost(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
 
-        available_nodes = set()
-        union_find = UnionFind(n)
         for e in edges:
             graph[e[0]].append((e[1], e[2]))
             graph[e[1]].append((e[0], e[2]))
-            available_nodes.add(e[0])
-            available_nodes.add(e[1])
-            union_find.union(e[0], e[1])
+        ans = defaultdict(lambda: (-1, set()))
 
-        ans = defaultdict(lambda: 2**30-1)
-        for e in edges:
-            ans[union_find.find(e[0])] &= e[2]
+        visited = defaultdict(bool)
+        def dfs(curr_v, vertexes_list):
+            nonlocal curr_and_val
+
+            if visited[curr_v]:
+                return
+            vertexes_list.add(curr_v)
+            visited[curr_v] = True
+
+            for e, v in graph[curr_v]:
+                dfs(e, vertexes_list)
+                curr_and_val &= v
+
+            return
 
         q_ans = []
 
         for q in query:
-            if union_find.find(q[0]) == union_find.find(q[1]):
-                q_ans.append(ans[union_find.find(q[0])])
+            if not visited[q[0]]:
+                vertexes_list = set()
+                curr_and_val = 2 ** 30 - 1
+                dfs(q[0], vertexes_list)
+                for v in vertexes_list:
+                    ans[v] = (curr_and_val, vertexes_list)
+            if q[1] in ans[q[0]][1]:
+                q_ans.append(ans[q[0]][0])
             else:
                 q_ans.append(-1)
 
         return q_ans
-
-
-class UnionFind:
-    def __init__(self, size):
-        self.parent = [i for i in range(size)]
-
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])  # Path compression
-        return self.parent[x]
-
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            self.parent[rootY] = rootX  # Attach one root to another
-            return True
-        return False
