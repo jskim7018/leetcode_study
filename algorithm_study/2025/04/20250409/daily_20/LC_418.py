0ab6e2271be6c8bd1542d1d36e6b67cce59d04d7
Jun Soo Kim <jskim7018@gmail.com>
2025-04-09 14:28:27 +0900

    20250409 - Codeforces Round 1016 1

--- algorithm_study/2025/04/20250409/daily_20/LC_418.py
+++ algorithm_study/2025/04/20250409/daily_20/LC_418.py
@@ -4,5 +4,28 @@ from typing import List
 class Solution:
     def wordsTyping(self, sentence: List[str], rows: int, cols: int) -> int:
         n = len(sentence)
+        s_total_length = n
 
+        for s in sentence:
+            s_total_length += len(s)
+
+        curr_idx = 0
+        ans = 0
+        for _ in range(rows):
+            curr_left_cols = cols
+            if curr_left_cols >= s_total_length-1:
+                ans += (curr_left_cols+1)//s_total_length * n
+                curr_left_cols += 1
+                curr_left_cols %= s_total_length
+                curr_left_cols -= 1
+
+            while curr_left_cols > 0:
+                if curr_left_cols >= len(sentence[curr_idx]):
+                    curr_left_cols -= len(sentence[curr_idx]) + 1
+                    ans += 1
+                    curr_idx += 1
+                    curr_idx %= n
+                else:
+                    break
+        ans //= n
         return ans
