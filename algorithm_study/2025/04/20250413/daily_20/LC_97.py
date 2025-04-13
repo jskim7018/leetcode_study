7798653a3855c4ac8151a507dc073e97f8ee7b46
Jun Soo Kim <jskim7018@gmail.com>
2025-04-13 23:55:22 +0900

    20250413 - Leetcode Study 6-2

--- algorithm_study/2025/04/20250413/daily_20/LC_97.py
+++ algorithm_study/2025/04/20250413/daily_20/LC_97.py
@@ -3,18 +3,20 @@ from functools import cache
 
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
 
         @cache
-        def solve(idx1, idx2, idx3) -> bool:
-            if idx1 >= len(s1) and idx2 >= len(s2) and idx3 >= len(s3):
+        def solve(idx1, idx2) -> bool:
+            if idx1 >= len(s1) and idx2 >= len(s2):
                 return True
 
             ret = False
-            if idx1 < len(s1) and idx3 < len(s3) and s1[idx1] == s3[idx3]:
-                ret |= solve(idx1 + 1, idx2, idx3 + 1)
-            if idx2 < len(s2) and idx3 < len(s3) and s2[idx2] == s3[idx3]:
-                ret |= solve(idx1, idx2 + 1, idx3 + 1)
+            if idx1 < len(s1) and s1[idx1] == s3[idx1+idx2]:
+                ret |= solve(idx1 + 1, idx2,)
+            if idx2 < len(s2) and s2[idx2] == s3[idx1+idx2]:
+                ret |= solve(idx1, idx2 + 1)
 
             return ret
 
-        return solve(0,0,0)
+        return solve(0,0)
